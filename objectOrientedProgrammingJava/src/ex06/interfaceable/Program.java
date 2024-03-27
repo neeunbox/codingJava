50a4dffdbc76f40eb4c9854e84fdef3ab523a3d1
SEUNG RAK <seungrak@SEUNGs-MacBook-Pro.local>
2024-03-28 07:01:53 +0900

    Modify interface read file

--- objectOrientedProgrammingJava/src/ex06/interfaceable/Program.java
+++ objectOrientedProgrammingJava/src/ex06/interfaceable/Program.java
@@ -1,11 +1,30 @@
 package ex06.interfaceable;
 
-public class Program {
-    public static void main(String[] args) {
-        A a = new A();
-        B b = new B();
-        a.setX(b);
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
+public class Program {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+        FileInputStream fis = new FileInputStream("src/ex06/interfaceable/setting.txt");
+        Scanner scan = new Scanner(fis);
+        String className = scan.nextLine();
+        scan.close();
+        fis.close();
+
+
+        Class<?> clazz = Class.forName(className);
+
+        A a = new A(); // A 클래스도 제네릭 타입을 사용하도록 수정
+
+        Constructor<?> constructor = clazz.getConstructor();
+
+        X x = (X) constructor.newInstance(); // 생성자를 이용하여 인스턴스 생성
+
+        a.setX(x);
         a.print();
     }
 }
