c41d32451f5fe8bc6ca61175571d1e0973f0dd9d
neeunbox <neeunbox@gmail.com>
2024-03-28 12:54:45 +0900

    Modify interface

--- GamePrj/GameFrame.java
+++ GamePrj/GameFrame.java
@@ -1,11 +1,64 @@
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class GameFrame extends Frame {
+import javax.swing.JOptionPane;
+
+public class GameFrame extends Frame implements WindowListener {
+
+    public GameFrame() {
+        //WindowListener listener = new GameWindowListner();
+        addWindowListener(this);
+        setVisible(true);
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
         g.drawRect(100, 100, 200, 100);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowOpened'");
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        JOptionPane.showMessageDialog(this, "Good Bye");
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowClosed'");
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowIconified'");
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowDeiconified'");
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowActivated'");
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'windowDeactivated'");
     }
 }
--- GamePrj/Program.java
+++ GamePrj/Program.java
@@ -3,8 +3,5 @@ import java.awt.event.WindowListener;
 public class Program {
     public static void main(String[] args) {
         GameFrame frame = new GameFrame();
-        WindowListener listener = new GameWindowListner();
-        frame.addWindowListener(listener);
-        frame.setVisible(true);
     }
 }
